ab60766dd36a8e9b84cfe81ccb53ea33d45ab308
Adam Gotterer <adam.gotterer@common.com>
2019-05-07 22:43:29 -0400

    Fixed spacing

--- golang/example.go
+++ golang/example.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"github.com/nilshell/xmlrpc"
 )
 
 func main() {
-    client, _ := xmlrpc.NewClient("http://localhost:8000", nil)
+	client, _ := xmlrpc.NewClient("http://localhost:8000", nil)
 	result := []interface{}{}
-	client.Call("test_mode", [2][2]int{{4,8}, {9,3}}, &result)
+	client.Call("test_mode", [2][2]int{{4, 8}, {9, 3}}, &result)
 
-    var boolRes interface{}
+	var boolRes interface{}
 
-    fmt.Printf("Someone on floor #4 requested to go to floor #8\n")
-	client.Call("service", [2]int{4,8}, &boolRes)
+	fmt.Printf("Someone on floor #4 requested to go to floor #8\n")
+	client.Call("service", [2]int{4, 8}, &boolRes)
 	client.Call("move", 2, &boolRes)
 	client.Call("move", 3, &boolRes)
 	client.Call("move", 4, &boolRes)
-    client.Call("pickup", nil, &boolRes)
+	client.Call("pickup", nil, &boolRes)
 	client.Call("move", 5, &boolRes)
 	client.Call("move", 6, &boolRes)
 	client.Call("move", 7, &boolRes)
 	client.Call("move", 8, &boolRes)
 	client.Call("dropoff", nil, &boolRes)
-    
-    fmt.Printf("Someone on floor #9 requested to go to floor #3\n")
-	client.Call("service", [2]int{9,3}, &boolRes)
+
+	fmt.Printf("Someone on floor #9 requested to go to floor #3\n")
+	client.Call("service", [2]int{9, 3}, &boolRes)
 	client.Call("move", 9, &boolRes)
-    client.Call("pickup", nil, &boolRes)
+	client.Call("pickup", nil, &boolRes)
 	client.Call("move", 8, &boolRes)
 	client.Call("move", 7, &boolRes)
 	client.Call("move", 6, &boolRes)
@@ -35,7 +35,7 @@ func main() {
 	client.Call("move", 4, &boolRes)
 	client.Call("move", 3, &boolRes)
 	client.Call("dropoff", nil, &boolRes)
-    
-    // Invalid jump fro floor 3 to 1
+
+	// Invalid jump fro floor 3 to 1
 	client.Call("move", 1, &boolRes)
 }
--- golang/logic.go
+++ golang/logic.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    
-    "github.com/nilshell/xmlrpc"
+	"fmt"
+	"time"
+
+	"github.com/nilshell/xmlrpc"
 )
 
 func main() {
-    var boolRes interface{}
-    var floor_requests [][2]int64
-    var res []interface{}
-    
-    client, _ := xmlrpc.NewClient("http://localhost:8000", nil)
-    client.Call("reset", nil, &boolRes)
+	var boolRes interface{}
+	var floor_requests [][2]int64
+	var res []interface{}
 
-    for {
-	client.Call("check_for_elevator_request", nil, &res)
-	
-	if len(res) == 2 {
-	    floor_request := [2]int64{}
-	    for i := range res {
-		floor_request[i] = res[i].(int64)
-	    }
+	client, _ := xmlrpc.NewClient("http://localhost:8000", nil)
+	client.Call("reset", nil, &boolRes)
 
-	    floor_requests = append(floor_requests, floor_request)
-	    fmt.Printf("Someone on floor #%d requested to go to floor #%d\n", floor_request[0], floor_request[1])
-	} else {
-	    // No new elevator requests
-	    fmt.Println(".")
+	for {
+		client.Call("check_for_elevator_request", nil, &res)
+
+		if len(res) == 2 {
+			floor_request := [2]int64{}
+			for i := range res {
+				floor_request[i] = res[i].(int64)
+			}
+
+			floor_requests = append(floor_requests, floor_request)
+			fmt.Printf("Someone on floor #%d requested to go to floor #%d\n", floor_request[0], floor_request[1])
+		} else {
+			// No new elevator requests
+			fmt.Println(".")
+		}
+
+		time.Sleep(1 * time.Second)
 	}
-
-	time.Sleep(1 * time.Second)
-    }
 }
